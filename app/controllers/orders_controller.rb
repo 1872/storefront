093c6d6f54245920b3285749b82cde9f120e1e53
Peter Jang <peter.x.jang@gmail.com>
2016-03-21 19:49:37 -0500

    saved subtotal, tax, total on order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.create(
       user_id: current_user.id,
       quantity: params[:quantity],
-      product_id: params[:product_id]
+      product_id: params[:product_id],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
     flash[:success] = "Order successfully created!"
     redirect_to "/orders/#{@order.id}"
