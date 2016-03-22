7d1ed25627135654e9f9b97bfb8bab2a232dceba
Peter Jang <peter.x.jang@gmail.com>
2016-03-21 19:30:55 -0500

    redirected the create order action to a show action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,15 @@
 class OrdersController < ApplicationController
   def create
-    Order.create(
+    @order = Order.create(
       user_id: current_user.id,
       quantity: params[:quantity]
     )
+    flash[:success] = "Order successfully created!"
+    redirect_to "/orders/#{@order.id}"
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render 'show.html.erb'
   end
 end
