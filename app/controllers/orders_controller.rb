c8f0d376a171dd03f339e883ee21fc4b6e76486e
Peter Jang <peter.x.jang@gmail.com>
2016-03-21 19:36:13 -0500

    saved product_id when creating a new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,8 @@ class OrdersController < ApplicationController
   def create
     @order = Order.create(
       user_id: current_user.id,
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      product_id: params[:product_id]
     )
     flash[:success] = "Order successfully created!"
     redirect_to "/orders/#{@order.id}"
