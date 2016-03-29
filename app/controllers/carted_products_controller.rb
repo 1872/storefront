2219851cc43a8f9a797837e18460478d265d3681
Peter Jang <peter.x.jang@gmail.com>
2016-03-28 19:38:36 -0500

    created way for user to remove items from cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -18,4 +18,11 @@ class CartedProductsController < ApplicationController
     )
     redirect_to "/carted_products"
   end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.update(status: "removed")
+    flash[:success] = "Product successfully removed!"
+    redirect_to "/carted_products"
+  end
 end
